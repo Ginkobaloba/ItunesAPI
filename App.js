f2ed167899ed7721d726a884ee002494fb889954
Ginkobaloba <Dramattick1@gmail.com>
2016-09-29 18:06:56 -0500

    worked with interface working ajax

--- App.js
+++ App.js
@@ -1,41 +1,51 @@
 $('#submit').click(function(){
-    var input = $("#command_line").val();
-		if (input == "?"){input = "?!"}
-		sendRequest(input)
-		console.log(input)
+    var inputTerm = $("#command_line").val();
+	var inputMedia =  $("#command_filter").val();
+		if (inputTerm == "?"){inputTerm = "?!"}
+		setupUrl(inputTerm, inputMedia);
 	$("#command_line").val("")
 });
  $("form").submit(function() {
-        var input = $("#command_line").val();
-		if (input == "?"){input = "?!"}
-		sendRequest(input)
-		console.log(input)
+        var inputTerm = $("#command_line").val();
+		var inputMedia =  $("#command_filter").val();
+		if (inputTerm == "?"){inputTerm = "?!"}
+		setupUrl(inputTerm, inputMedia);
 	$("#command_line").val("")
  });
  
- 
- 
- function handleResponse(data){
-	 
-	 console.log(data)
-	 console.log(data)
-	 
- }
- 
- 
- 
- 
- 
- 
- function sendRequest(input){
-	 var unencoded = "http://itunes.apple.com/search?term="+input+"&callback=handleResponse";
-	 var encoded = encodeURI(unencoded);
-	 $('head').append('<script src="' + encoded +'"></script>');
-    //$.ajax({
-        //url: encoded,
-       // dataType: 'JSONP'
-    //})
-    //.done(function(data) { console.log(data); })
-    //.fail(function(data) { console.log(data); })
- };
 
+function setupUrl(inputTerm, inputMedia){
+	
+	toBeEncoded = {
+		
+		term:inputTerm,
+		media:inputMedia,
+		callback:'handleReturnedData'
+	};
+EncodeUrl(toBeEncoded)
+}
+	
+	
+	
+function EncodeUrl(toBeEncoded){
+	var encoded = "";
+	for (var item in toBeEncoded){
+	encoded = encoded + encodeURIComponent(item) + '=' + encodeURIComponent(toBeEncoded[item]) + '&';
+	}
+	
+	encoded = encoded.substring(0, encoded.length-1);
+	
+	api = 'https://itunes.apple.com/search?'
+	apiRequest = api + encoded
+	toBeAppened = '<script src=' + apiRequest +'><\/script>'
+	jQuery('body').append(toBeAppened);
+}
+
+function handleReturnedData(Results){
+	console.log(Results)
+	for (var item in Results){
+		
+	$('Song: ' + Results[item].trackName).clone().insertBefore("#placeholder").fadeIn(1000)
+	}
+	
+}
